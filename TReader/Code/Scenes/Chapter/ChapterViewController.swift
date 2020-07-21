5b56c0a8917416768258360da5c460ef246facde
Stotion1 <dev2@stotion.com>
2020-07-21 22:36:19 +0300

    Move code to separate class.

--- TReader/Code/Scenes/Chapter/ChapterViewController.swift
+++ TReader/Code/Scenes/Chapter/ChapterViewController.swift
@@ -10,22 +10,12 @@ import UIKit
 import RxSwift
 import RxCocoa
 import WebKit
-import MenuItemKit
-
-class WebView: WKWebView {
-    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
-        if action == #selector(self.copy(_:)) {
-            return true
-        }
-        return false
-    }
-}
 
 class ChapterViewController: UIViewController {
     var viewModel: ChapterViewModel!
 
     // swiftlint:disable force_cast
-    private var webView: WKWebView { view as! WKWebView }
+    private var webView: WebView { view as! WebView }
     // swiftlint:enable force_cast
 
     private var openChapterSubject = PublishSubject<(Int, Int)>()
@@ -41,7 +31,6 @@ class ChapterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setDefaultMenu()
         bindViewModel()
     }
 
@@ -67,46 +56,6 @@ class ChapterViewController: UIViewController {
             .drive()
             .disposed(by: rx.disposeBag)
     }
-
-    func setDefaultMenu() {
-//        UIMenuController.shared.setDefaultMenu { highlight in
-//            if highlight {
-//                self.highlight()
-//            } else {
-//                self.note()
-//            }
-//        }
-
-        let highlight = UIMenuItem(title: "Highlight", action: #selector(self.highlight))
-        let note = UIMenuItem(title: "Note", action: #selector(self.note))
-        UIMenuController.shared.menuItems = [highlight, note]
-    }
-
-    @objc func highlight() {
-        let js = "highlight(\"pink\");"
-        webView.evaluateJavaScript(js) { (obj, err) in
-            if let err = err { print(err) }
-            if let obj = obj { print(obj) }
-        }
-        UIMenuController.shared.hideMenu()
-    }
-
-    @objc func note() {
-        var rect = UIMenuController.shared.menuFrame
-        rect.origin.y = rect.maxY
-        UIMenuController.shared.hideMenu()
-        
-        UIMenuController.shared.setColorsMenu { [weak self] (color: String) in
-            UIMenuController.shared.hideMenu()
-            let js = "highlight(\"\(color)\");"
-            self?.setDefaultMenu()
-            self?.webView.evaluateJavaScript(js) { (obj, err) in
-                if let err = err { print(err) }
-                if let obj = obj { print(obj) }
-            }
-        }
-        UIMenuController.shared.showMenu(from: webView, rect: rect)
-    }
 }
 
 extension ChapterViewController: WKUIDelegate {
@@ -130,22 +79,5 @@ extension ChapterViewController: WKNavigationDelegate {
         } else {
             decisionHandler(.allow)
         }
-    }
-}
-
-extension UIMenuController {
-    func setDefaultMenu(action: @escaping (Bool) -> Void) {
-        let highlight = UIMenuItem(title: "Highlight") { _ in action(true) }
-        let note = UIMenuItem(title: "Note") { _ in action(false) }
-        UIMenuController.shared.menuItems = [highlight, note]
-    }
-
-    func setColorsMenu(action: @escaping (String) -> Void) {
-        let blue = UIMenuItem(title: "Blue", image: #imageLiteral(resourceName: "EllipseBlue")) { _ in action("blue") }
-        let green = UIMenuItem(title: "Green", image: #imageLiteral(resourceName: "EllipseGreen")) { _ in action("green") }
-        let purple = UIMenuItem(title: "Purple", image: #imageLiteral(resourceName: "EllipsePurple")) { _ in action("purple") }
-        let red = UIMenuItem(title: "Red", image: #imageLiteral(resourceName: "EllipseRed")) { _ in action("red") }
-        let yellow = UIMenuItem(title: "Yellow", image: #imageLiteral(resourceName: "EllipseYellow")) { _ in action("yellow") }
-        UIMenuController.shared.menuItems = [blue, green, purple, red, yellow]
     }
 }
---
+++ TReader/Code/Scenes/Chapter/WebView.swift
@@ -0,0 +1,84 @@
+//
+//  WebView.swift
+//  TReader
+//
+//  Created by tadas on 2020-07-21.
+//  Copyright © 2020 Scale3C. All rights reserved.
+//
+
+import Foundation
+import WebKit
+import MenuItemKit
+
+private struct ColorModel {
+    let title: String
+    let color: String
+    let img: UIImage
+}
+
+class WebView: WKWebView {
+
+    private lazy var defaultColor = colors[0]
+    private let colors = [ColorModel(title: "Yellow", color: "#FFFACD", img: #imageLiteral(resourceName: "EllipseYellow")),
+                          ColorModel(title: "Green", color: "#E3FFCD", img: #imageLiteral(resourceName: "EllipseGreen")),
+                          ColorModel(title: "Blue", color: "#CEF0FF", img: #imageLiteral(resourceName: "EllipseBlue")),
+                          ColorModel(title: "Red", color: "#FFE1EC", img: #imageLiteral(resourceName: "EllipseRed")),
+                          ColorModel(title: "Purple", color: "#CDD2FF", img: #imageLiteral(resourceName: "EllipsePurple"))]
+
+    override init(frame: CGRect, configuration: WKWebViewConfiguration) {
+        super.init(frame: frame, configuration: configuration)
+        setDefaultMenu()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        switch action {
+        case #selector(self.highlight): return true
+        case #selector(self.note): return true
+        case #selector(self.copy(_:)): return (UIMenuController.shared.menuItems?.count ?? 0 > 2 ? false : true)
+        default: return false
+        }
+    }
+
+    private func setDefaultMenu() {
+        let highlight = UIMenuItem(title: "Highlight", action: #selector(self.highlight))
+        let note = UIMenuItem(title: "Note", action: #selector(self.note))
+        UIMenuController.shared.menuItems = [highlight, note]
+    }
+
+    @objc private func highlight() {
+        let js = javaScript(defaultColor)
+        evaluateJavaScript(js) { (obj, err) in
+            if let err = err { print(err) }
+            if let obj = obj { print(obj) }
+        }
+        UIMenuController.shared.hideMenu()
+    }
+
+    @objc private func note() {
+        var rect = UIMenuController.shared.menuFrame
+        rect.origin.y = rect.maxY
+        UIMenuController.shared.hideMenu()
+
+        let action = { [weak self] (model: ColorModel) in
+            guard let `self` = self else { return }
+            UIMenuController.shared.hideMenu()
+            let js = self.javaScript(model)
+            self.defaultColor = model
+            self.setDefaultMenu()
+            self.evaluateJavaScript(js) { (obj, err) in
+                if let err = err { print(err) }
+                if let obj = obj { print(obj) }
+            }
+        }
+        UIMenuController.shared.menuItems = colors.map { model in UIMenuItem(title: model.title, image: model.img) { _ in action(model) } }
+        UIMenuController.shared.showMenu(from: self, rect: rect)
+    }
+
+    private func javaScript(_ model: ColorModel) -> String {
+        return "highlight(\"\(model.color)\");window.getSelection().removeAllRanges();"
+    }
+}
