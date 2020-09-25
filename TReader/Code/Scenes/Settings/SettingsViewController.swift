75e1e76cfea57401c5d4029b861f27646a1c3abd
Stotion1 <dev2@stotion.com>
2020-09-26 02:18:53 +0300

    Layout settings.

--- TReader/Code/Scenes/Settings/SettingsViewController.swift
+++ TReader/Code/Scenes/Settings/SettingsViewController.swift
@@ -16,7 +16,7 @@ class SettingsViewController: UIViewController, MFMailComposeViewControllerDeleg
     var viewModel: SettingsViewModel!
 
     @IBOutlet weak var logInButton: UIButton!
-    @IBOutlet weak var inputTextView: UITextView!
+//    @IBOutlet weak var inputTextView: UITextView!
     @IBOutlet weak var saveButton: UIButton!
     @IBOutlet weak var bottomLayoutConstraint: NSLayoutConstraint!
 
@@ -30,10 +30,10 @@ class SettingsViewController: UIViewController, MFMailComposeViewControllerDeleg
     func configureView() {
         let cornerRadius: CGFloat = 3.0
 
-        inputTextView.layer.cornerRadius = cornerRadius
-        inputTextView.layer.borderWidth = 1
-        inputTextView.layer.borderColor = UIColor(hexString: "#E6E6E6")!.cgColor
-        inputTextView.textContainerInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+//        inputTextView.layer.cornerRadius = cornerRadius
+//        inputTextView.layer.borderWidth = 1
+//        inputTextView.layer.borderColor = UIColor(hexString: "#E6E6E6")!.cgColor
+//        inputTextView.textContainerInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
 
         saveButton.layer.cornerRadius = cornerRadius
     }
@@ -59,11 +59,11 @@ class SettingsViewController: UIViewController, MFMailComposeViewControllerDeleg
             .drive()
             .disposed(by: rx.disposeBag)
 
-        inputTextView.rx.text
-            .asDriver()
-            .map { $0 != nil && $0!.isEmpty == false }
-            .drive(saveButton.rx.isEnabled)
-            .disposed(by: rx.disposeBag)
+//        inputTextView.rx.text
+//            .asDriver()
+//            .map { $0 != nil && $0!.isEmpty == false }
+//            .drive(saveButton.rx.isEnabled)
+//            .disposed(by: rx.disposeBag)
 
         RxKeyboard.instance
             .visibleHeight
@@ -74,13 +74,18 @@ class SettingsViewController: UIViewController, MFMailComposeViewControllerDeleg
     }
 
     @IBAction func composeEmail() {
-        if MFMailComposeViewController.canSendMail() {
-            let mailComposeViewController = MFMailComposeViewController()
-            mailComposeViewController.mailComposeDelegate = self
-            mailComposeViewController.setToRecipients(["abc@abc.abc"])
-            mailComposeViewController.setSubject("Subject")
-            mailComposeViewController.setMessageBody(inputTextView.text, isHTML: false)
-            present(mailComposeViewController, animated: true, completion: nil)
+//        if MFMailComposeViewController.canSendMail() {
+//            let mailComposeViewController = MFMailComposeViewController()
+//            mailComposeViewController.mailComposeDelegate = self
+//            mailComposeViewController.setToRecipients(["abc@abc.abc"])
+//            mailComposeViewController.setSubject("Subject")
+//            mailComposeViewController.setMessageBody(inputTextView.text, isHTML: false)
+//            present(mailComposeViewController, animated: true, completion: nil)
+//        }
+
+        let email = "foo@bar.com"
+        if let url = URL(string: "mailto:\(email)") {
+            UIApplication.shared.open(url)
         }
     }
 
